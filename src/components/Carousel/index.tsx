c8ad61563ccc3a0583ff4984b121a34eb0286714
Mark Carson <markcarson121@gmail.com>
2024-08-12 12:48:52 -0300

    added props to Carousel component

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,3 +1,11 @@
-export function Carousel() {
-  return <h1>Carousel</h1>;
+import clsx from "clsx";
+
+type CarouselProps = {
+  className?: string;
+};
+
+export function Carousel(props: CarouselProps) {
+  const { className } = props;
+
+  return <h1 className={clsx(className)}>Carousel</h1>;
 }
