6deda1f0ff48924b98977c5ce27ad777c6e4a3fe
Mark Carson <markcarson121@gmail.com>
2024-08-12 12:26:56 -0300

    created Navbar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,43 @@
+export function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4 w-full">
+      <div className="flex justify-between items-center">
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              SHOP
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              SIZING AND FIT
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              CUSTOMER CARE
+            </a>
+          </li>
+        </ul>
+        <h1 className="text-white text-xl font-bold">E-Commerce</h1>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              SEARCH
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              USER
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-gray-300 hover:text-white">
+              CART
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
