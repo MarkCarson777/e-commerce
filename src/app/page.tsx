3eb3849aa7778ebfd8e09ddef20830dc9723c2ec
Mark Carson <markcarson121@gmail.com>
2024-08-12 12:42:01 -0300

    added carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Carousel } from "../components/Carousel";
 import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center">
       <Navbar />
-      <h1>Home</h1>
+      <Carousel />
     </main>
   );
 }
