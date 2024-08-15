81210d0886c10431ac4f3ab6d9936d70c881a162
Mark Carson <markcarson121@gmail.com>
2024-08-15 15:42:40 -0300

    integrated formik into signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,54 +1,47 @@
 "use client";
 
-import { useState } from "react";
-import { Formik, Form } from "formik";
+import { Field, Formik, Form } from "formik";
 import { signIn } from "@/firebase/auth";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const router = useRouter();
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const { result, error } = await signIn(email, password);
-
-    if (error) {
-      return console.log("Error signing in", error);
-    }
-
-    console.log("Successfully signed in", result);
-    return router.push("/");
-  };
-
   return (
-    <form onSubmit={onSubmit} className="form">
-      <label htmlFor="email">
-        <p>Email</p>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email..."
-          autoComplete="email"
-        />
-      </label>
-      <label htmlFor="password">
-        <p>Password</p>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password..."
-          autoComplete="new-password"
-        />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          const { result, error } = await signIn(values.email, values.password);
+
+          if (error) {
+            return console.log("Error signing in", error);
+          }
+
+          console.log("Successfully signed in", result);
+          return router.push("/");
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="flex flex-col">
+            <Field
+              name="email"
+              type="email"
+              placeholder="Email..."
+              autoComplete="email"
+            />
+            <Field
+              name="password"
+              type="password"
+              placeholder="Password..."
+              autoComplete="current-password"
+            />
+            <button type="submit" disabled={isSubmitting}>
+              Sign in
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
