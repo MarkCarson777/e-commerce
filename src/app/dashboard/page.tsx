540e2c650ccf8cd7d00e4f669adeaf035828f720
Mark Carson <markcarson121@gmail.com>
2024-08-19 16:57:05 -0300

    built very basic ProductCard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { AuthRoute } from "@/containers/AuthRoute";
+
 import { useProductContext } from "@/context/ProductContext";
 
-import placeholder from "/public/images/carouselOne.jpg";
+import { ProductCard } from "@/components/ProductCard";
 
 function Page() {
   const { products } = useProductContext();
@@ -14,17 +14,11 @@ function Page() {
     <>
       <Link href="/dashboard/create">Add product</Link>
       <h1>All products</h1>
-      {products.map((product, index) => (
-        <div key={index}>
-          <span>{product.name}</span>
-          <Image
-            src={product.image || placeholder}
-            alt={product.name}
-            height={200}
-            width={200}
-          />
-        </div>
-      ))}
+      <div className="relative grid grid-cols-4 gap-3">
+        {products.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
+      </div>
     </>
   );
 }
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,11 +1,33 @@
 import clsx from "clsx";
 
+import Image from "next/image";
+
+import placeholder from "/public/images/carouselOne.jpg";
+
+import { Product } from "@/types";
+
 type ProductCardProps = {
+  product: Product;
   className?: string;
 };
 
 export function ProductCard(props: ProductCardProps) {
-  const { className } = props;
+  const { product, className } = props;
 
-  return <div className={clsx(className)}>ProductCard</div>;
+  return (
+    <article className={clsx("flex flex-col border-2", className)}>
+      <figure className="relative h-64 w-full">
+        <Image
+          src={product.image || placeholder}
+          alt={product.name}
+          fill
+          sizes="20vw"
+          priority
+        />
+      </figure>
+      <h2>{product.name}</h2>
+      <p>£{product.price}</p>
+      <p>{product.description}</p>
+    </article>
+  );
 }
