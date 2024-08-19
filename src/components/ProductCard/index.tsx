d37c40a1e7a93e676754e319594ad481839aecc5
Mark Carson <markcarson121@gmail.com>
2024-08-19 17:04:46 -0300

    added routing and page template for product show page

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 
 import Image from "next/image";
+import Link from "next/link";
 
 import placeholder from "/public/images/carouselOne.jpg";
 
@@ -15,19 +16,21 @@ export function ProductCard(props: ProductCardProps) {
   const { product, className } = props;
 
   return (
-    <article className={clsx("flex flex-col border-2", className)}>
-      <figure className="relative h-64 w-full">
-        <Image
-          src={product.image || placeholder}
-          alt={product.name}
-          fill
-          sizes="20vw"
-          priority
-        />
-      </figure>
-      <h2>{product.name}</h2>
-      <p>£{product.price}</p>
-      <p>{product.description}</p>
-    </article>
+    <Link href={`/products/${product.id}`}>
+      <article className={clsx("flex flex-col border-2", className)}>
+        <figure className="relative h-64 w-full">
+          <Image
+            src={product.image || placeholder}
+            alt={product.name}
+            fill
+            sizes="20vw"
+            priority
+          />
+        </figure>
+        <h2>{product.name}</h2>
+        <p>£{product.price}</p>
+        <p>{product.description}</p>
+      </article>
+    </Link>
   );
 }
