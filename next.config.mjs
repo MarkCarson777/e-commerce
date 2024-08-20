20a1613842b4bd1ef796ab8c022f00942eaf3ee2
Mark Carson <markcarson121@gmail.com>
2024-08-20 14:58:04 -0300

    configured svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: {
+        loader: "@svgr/webpack",
+        options: {
+          svgoConfig: {
+            plugins: [
+              {
+                name: "preset-default",
+                params: {
+                  overrides: {
+                    removeViewBox: false,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
